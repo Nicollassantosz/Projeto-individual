57b80cde7183d3f0f1727e6e8ad3cc8ea1011223
Nicollassantosz <nicollas.santos@sptech.school>
2024-05-15 23:36:53 -0300

    Adições na tela inicial

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Data da próxima Copa do Mundo 2026
-var nextWorldCupDate = new Date("2026-06-11T00:00:00Z");
+var dataproximacopa = new Date("2026-06-11T00:00:00Z");
 
-function updateCountdown() {
+function atualizarcontador() {
     var now = new Date();
-    var timeDiff = nextWorldCupDate - now;
+    var temporestante = dataproximacopa - now;
 
     // Calcular dias, horas, minutos e segundos restantes
-    var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    var dias = Math.floor(temporestante / (1000 * 60 * 60 * 24));
+    var horas = Math.floor((temporestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutos = Math.floor((temporestante % (1000 * 60 * 60)) / (1000 * 60));
+    var segundos = Math.floor((temporestante % (1000 * 60)) / 1000);
 
     // Atualizar o elemento HTML com a contagem regressiva
-    document.getElementById("countdown").innerHTML = `Faltam ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos para a próxima Copa do Mundo!`;
+    document.getElementById("countdown").innerHTML = `<div class="align"><div class = quadrado>${dias}</div> :  <div class = quadrado>${horas}</div>  : <div class = quadrado>${minutos}</div>  : <div class = quadrado>${segundos}</div> </div> 
+ <div class = retangulo>  <span id="span_cowndown">DIAS        HORAS  -----    MIN   -----   SEG </span> </div> `;
 }
 
 // Atualizar a contagem regressiva a cada segundo
-setInterval(updateCountdown, 1000);
+setInterval(atualizarcontador, 1000);
 
 // Chamar a função de atualização inicialmente para evitar atrasos na exibição
-updateCountdown();
+atualizarcontador();
